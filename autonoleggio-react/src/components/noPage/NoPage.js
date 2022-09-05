81f57348aa30ebd7acaac2602ce1a988f071688a
gdantimi <gianluigi.dantimi@gmail.com>
2022-09-05 15:20:01 +0200

    Update NoPage.js

---
+++ autonoleggio-react/src/components/noPage/NoPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './NoPage.css';
+import Bottone from '../bottone/Bottone';
+
+class NoPage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h2>La pagina non è stata trovata</h2>
+
+                {/*
+                <Bottone
+                    testo='Torna alla home'
+                    linkBottone='/home'>
+                </Bottone>
+                */}
+            </div>
+        );
+    }
+}
+
+export default NoPage;
