e5f8ca8f0928f9800f59b84798560ff77dd7a6ab
gdantimi <gianluigi.dantimi@gmail.com>
2022-09-06 13:35:38 +0200

    update header

--- autonoleggio-react/src/components/header/Header.js
+++ autonoleggio-react/src/components/header/Header.js
@@ -4,6 +4,8 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import logo from '../../media/logoNoleggiova-black.png';
+import logoHover from '../../media/logoNoleggiova-orange.png';
+
 
 class Header extends React.Component {
 
@@ -12,24 +14,24 @@ class Header extends React.Component {
             <div className='header'>
                 <Navbar>
                     <Container>
-                        <Navbar.Brand /* href="/home" */ className='elem-left'>
+                        <Navbar.Brand href="/home" className='elem-left'>
                             <img
                                 className='logo'
                                 src={logo}
-                                width="40"
-                                height="40"
+                                onMouseOver={e => e.currentTarget.src = logoHover}
+                                onMouseOut={e => e.currentTarget.src = logo}
                             />
 
-                            <Nav.Link className="navLink navLink-left"/* href="/home" */>Noleggiovà</Nav.Link>
+                            <Nav.Link className="navLink navLink-left" href="/home" >Noleggiovà</Nav.Link>
                         </Navbar.Brand>
 
                         <div className='elem-right'>
                             <Nav >
                                 <div>
-                                    <Nav.Link className="navLink navLink-right"/* href="/login" */>Login</Nav.Link>
-                                    <Nav.Link className="navLink navLink-right" /* href="/listaAuto" */>Auto</Nav.Link>
-                                    <Nav.Link className="navLink navLink-right" /* href="/chiSiamo" */>Chi siamo</Nav.Link>
-                                    <Nav.Link className="navLink navLink-right" /* href="/contatti" */>Contatti</Nav.Link>
+                                    <Nav.Link className="navLink navLink-right" href="/login" >Login</Nav.Link>
+                                    <Nav.Link className="navLink navLink-right" href="/listaAuto" >Auto</Nav.Link>
+                                    <Nav.Link className="navLink navLink-right" href="/chiSiamo" >Chi siamo</Nav.Link>
+                                    <Nav.Link className="navLink navLink-right" href="/contatti" >Contatti</Nav.Link>
                                 </div>
                             </Nav>
                         </div>
