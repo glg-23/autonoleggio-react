5ee86b8587c3ee0d35391cdb3687173ed01c63f3
gdantimi <gianluigi.dantimi@gmail.com>
2022-09-06 11:00:10 +0200

    update app.js - add home

--- autonoleggio-react/src/App.js
+++ autonoleggio-react/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import Home from './components/home/Home';
 
 function App() {
 
   return (
     <div className="App">
         <Header></Header>
+        <Home></Home>
         <Footer></Footer>
     </div>
   );
