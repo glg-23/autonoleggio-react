b311bcda02f0e13a58b7385de95c1811b78a0ed3
gdantimi <gianluigi.dantimi@gmail.com>
2022-09-06 12:58:51 +0200

    update header css

--- autonoleggio-react/src/components/header/Header.js
+++ autonoleggio-react/src/components/header/Header.js
@@ -3,16 +3,15 @@ import './Header.css';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import logo from '../../media/carlogo.png';
+import logo from '../../media/logoNoleggiova-white.png';
 
 class Header extends React.Component {
 
     render() {
         return (
             <div className='header'>
-                <Navbar className='nav'>
+                <Navbar>
                     <Container>
-
                         <Navbar.Brand /* href="/home" */ className='elem-left'>
                             <img
                                 className='logo'
@@ -21,20 +20,19 @@ class Header extends React.Component {
                                 height="40"
                             />
 
-                            <Nav.Link className="navLink" /* href="/home" */>Noleggiovà</Nav.Link>
+                            <Nav.Link className="navLink navLink-left"/* href="/home" */>Noleggiovà</Nav.Link>
                         </Navbar.Brand>
 
                         <div className='elem-right'>
                             <Nav >
                                 <div>
-                                    <Nav.Link className="navLink" /* href="/login" */>Login</Nav.Link>
-                                    <Nav.Link className="navLink" /* href="/listaAuto" */>Auto</Nav.Link>
-                                    <Nav.Link className="navLink" /* href="/chiSiamo" */>Chi siamo</Nav.Link>
-                                    <Nav.Link className="navLink" /* href="/contatti" */>Contatti</Nav.Link>
+                                    <Nav.Link className="navLink navLink-right"/* href="/login" */>Login</Nav.Link>
+                                    <Nav.Link className="navLink navLink-right" /* href="/listaAuto" */>Auto</Nav.Link>
+                                    <Nav.Link className="navLink navLink-right" /* href="/chiSiamo" */>Chi siamo</Nav.Link>
+                                    <Nav.Link className="navLink navLink-right" /* href="/contatti" */>Contatti</Nav.Link>
                                 </div>
                             </Nav>
                         </div>
-
                     </Container>
                 </Navbar>
             </div>
