a1f1d27f328dc2877dfa67da7be3192c7ec19532
gdantimi <gianluigi.dantimi@gmail.com>
2022-09-07 10:22:23 +0200

    update media inCostruzione

--- autonoleggio-react/src/components/inCostruzione/InCostruzione.js
+++ autonoleggio-react/src/components/inCostruzione/InCostruzione.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './InCostruzione.css';
 import Bottone from '../bottone/Bottone';
+import logoCostruzione from '../../media/inCostruzione.png';
 
 class InCostruzione extends React.Component {
 
     render() {
         return (
             <div className='inCostruzione'>
+                <img
+                    className='logoCostruzione'
+                    src={logoCostruzione}
+                />
+
                 <h2 className='titoloInCostruzione'>Pagina in costruzione</h2>
 
                 {
