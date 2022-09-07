a1efcdd701a76306da5d044a20aa9e1d2bdca1ac
gdantimi <gianluigi.dantimi@gmail.com>
2022-09-07 10:53:24 +0200

    update router

--- autonoleggio-react/src/components/router/Router.js
+++ autonoleggio-react/src/components/router/Router.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from '../home/Home';
 import Info from '../informazioni/Informazioni'
+
+import ListaAuto from '../listaAuto/ListaAuto'
+import ChiSiamo from '../chiSiamo/ChiSiamo'
+import Contatti from '../contatti/Contatti'
+
 import InCostruzione from '../inCostruzione/InCostruzione';
 import NoPage from '../noPage/NoPage';
 
@@ -12,14 +17,13 @@ class Router extends React.Component {
             <BrowserRouter>
                 <Routes>
                     <Route index path="home" element={<Home />} />
-                    <Route index path="" element={<Home />} />
-
+                    <Route index path="" element={<Info />} />
                     <Route path="informazioni" element={<Info />} />
 
                     <Route path="login" element={<InCostruzione />} />
-                    <Route path="listaAuto" element={<InCostruzione />} />
-                    <Route path="chiSiamo" element={<InCostruzione />} />
-                    <Route path="contatti" element={<InCostruzione />} />
+                    <Route path="listaAuto" element={<ListaAuto />} />
+                    <Route path="chiSiamo" element={<ChiSiamo />} />
+                    <Route path="contatti" element={<Contatti />} />
 
                     <Route path="*" element={<NoPage />} />
                 </Routes>
