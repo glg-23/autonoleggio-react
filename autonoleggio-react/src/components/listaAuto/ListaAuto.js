e03762fe13998d657a5b5c61ba739fc4a669966e
gdantimi <gianluigi.dantimi@gmail.com>
2022-09-07 10:52:53 +0200

    init listaAuto

---
+++ autonoleggio-react/src/components/listaAuto/ListaAuto.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ListaAuto.css';
+
+class ListaAuto extends React.Component {
+
+    render() {
+        return (
+            <div className='sfondoListaAuto'>
+            </div>
+        );
+    }
+}
+
+export default ListaAuto;
