0ed209b1f59424f52ed75336b49cce118cfe15b3
gdantimi <gianluigi.dantimi@gmail.com>
2022-09-07 10:52:28 +0200

    int chiSiamo

---
+++ autonoleggio-react/src/components/chiSiamo/ChiSiamo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ChiSiamo.css';
+
+class ChiSiamo extends React.Component {
+
+    render() {
+        return (
+            <div className='sfondoChiSiamo'>
+            </div>
+        );
+    }
+}
+
+export default ChiSiamo;
