b5244d9d5dd815b22b08fff89678c4acc435f888
gdantimi <gianluigi.dantimi@gmail.com>
2022-09-07 10:52:40 +0200

    init contatti

---
+++ autonoleggio-react/src/components/contatti/Contatti.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Contatti.css';
+
+class Contatti extends React.Component {
+
+    render() {
+        return (
+            <div className='sfondoContatti'>
+            </div>
+        );
+    }
+}
+
+export default Contatti;
