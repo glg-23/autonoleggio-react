a102ad4cabddb4749e27bf4e8cd8935088e8dd7f
gdantimi <gianluigi.dantimi@gmail.com>
2022-09-07 10:22:32 +0200

    update media noPage

--- autonoleggio-react/src/components/noPage/NoPage.js
+++ autonoleggio-react/src/components/noPage/NoPage.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './NoPage.css';
 import Bottone from '../bottone/Bottone';
+import logoNoPage from '../../media/noPage.png';
 
 class NoPage extends React.Component {
 
     render() {
         return (
             <div className='noPage'>
+                <img
+                    className='logoNoPage'
+                    src={logoNoPage}
+                />
                 <h2 className='titoloNoPage'>Pagina non trovata</h2>
 
                 {
