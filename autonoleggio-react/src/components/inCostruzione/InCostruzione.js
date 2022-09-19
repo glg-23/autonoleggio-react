008cd733ff2ff5d640532c4539614ea4d9da056b
gdantimi <gianluigi.dantimi@gmail.com>
2022-09-19 11:38:35 +0200

    add box inCostruzione

--- autonoleggio-react/src/components/inCostruzione/InCostruzione.js
+++ autonoleggio-react/src/components/inCostruzione/InCostruzione.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import './InCostruzione.css';
 import Bottone from '../bottone/Bottone';
-import logoCostruzione from '../../media/icon/inCostruzione.png';
+import logoCostruzione from '../../media/icon/inCostruzione-white.png';
 
 class InCostruzione extends React.Component {
 
     render() {
         return (
-            <div className='inCostruzione'>
-                <img
-                    className='logoCostruzione'
-                    src={logoCostruzione}
-                />
+            <div className='containerCostruzione'>
+                <div className='boxCostruzione'>
+                    <img
+                        className='logoCostruzione'
+                        src={logoCostruzione}
+                        alt={"in costruzione"}
+                    />
 
-                <h2 className='titoloInCostruzione'>Pagina in costruzione</h2>
+                    <h2 className='titoloInCostruzione'>Pagina in costruzione</h2>
 
-                {
-                    <Bottone
-                        testo='Torna alla home'
-                        linkBottone='/home'>
-                    </Bottone>
-                }
+                    {
+                        <Bottone
+                            testo='Torna alla home'
+                            linkBottone='/home'>
+                        </Bottone>
+                    }
+                </div>
             </div>
         );
     }
