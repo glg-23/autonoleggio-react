bf54ceee4b7cdcf6bc9b3ffc0999b5fba93507a9
gdantimi <gianluigi.dantimi@gmail.com>
2022-09-19 12:36:02 +0200

    init login

---
+++ autonoleggio-react/src/components/login/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Login.css';
+import InCostruzione from '../inCostruzione/InCostruzione';
+
+class Login extends React.Component {
+
+    render() {
+        return (
+            <div className='sfondoLogin'>
+                <InCostruzione>
+                </InCostruzione>
+            </div>
+        );
+    }
+}
+
+export default Login;
